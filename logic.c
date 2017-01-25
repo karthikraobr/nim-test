64173465504423bb8b81b6374c18ce9266c95206
Bekal Radhakrishna <k.bekalradhakrishna@wincor-nixdorf.com>
2017-01-25 16:48:13 +0100

    Bla alb

---
+++ logic.c
@@ -0,0 +1,14 @@
+int addThreeIntegers(int a, int b, int c)
+{
+  return a + b + c;
+}
+
+int addTwoIntegers(int a,int b)
+{
+    return a + b;
+}
+
+char* returnString(char str[])
+{
+    return str;
+}
